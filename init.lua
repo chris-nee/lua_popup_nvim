e32bee63043f83f1f3aa28424adec60d20556781
chris-nee <lingngeehung@gmail.com>
2025-01-26 19:05:00 +0800

    rename

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 -- Module for managing pop up windows in NeoVim
+
 local M = {}
 
 
@@ -45,7 +46,7 @@ M.setup = function(opts)
     vim.api.nvim_set_keymap(
         "n",
         "<leader>p",
-        ":lua require('test').open_popup({ title = 'Pop up is here!', content = {'Welcome', 'Start Coding'} })<CR>",
+        ":lua require('lua_popup_nvim').open_popup({ title = 'Pop up is here!', content = {'Welcome', 'Start Coding'} })<CR>",
         { noremap = true, silent = true }
     )
 end
